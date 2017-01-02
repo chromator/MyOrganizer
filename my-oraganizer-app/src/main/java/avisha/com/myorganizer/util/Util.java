cb7403b614b36abb1551c738e9ebe94c8d9dddb8
Swaroop Kulkarni <skulw@allstate.com>
2017-01-02 17:42:18 -0600

    Multiple changes. Material theme applied.

---
+++ my-oraganizer-app/src/main/java/avisha/com/myorganizer/util/Util.java
@@ -0,0 +1,33 @@
+package avisha.com.myorganizer.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by skulw on 1/2/17.
+ */
+public class Util {
+    public static String toTextDate(int i, int i1, int i2) {
+        String txtDate = null;
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, i);
+        calendar.set(Calendar.MONTH, i1);
+        calendar.set(Calendar.DAY_OF_MONTH, i2);
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
+        dateFormat.applyPattern("EEE, MMM dd yyyy");
+        txtDate = dateFormat.format(calendar.getTime());
+        return txtDate;
+    }
+
+    public static String toTextTime(int i, int i1) {
+        String textTime = null;
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, i);
+        calendar.set(Calendar.MINUTE, i1);
+        SimpleDateFormat dateFormat = new SimpleDateFormat();
+        dateFormat.applyPattern("KK:mm a");
+        textTime = dateFormat.format(calendar.getTime());
+        return textTime;
+    }
+}
--- my-oraganizer-app/src/main/java/avisha/com/myorganizer/view/NewTaskActivity.java
+++ my-oraganizer-app/src/main/java/avisha/com/myorganizer/view/NewTaskActivity.java
@@ -1,24 +1,35 @@
 package avisha.com.myorganizer.view;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.pm.PackageManager;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TimePicker;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import avisha.com.myorganizer.R;
 import avisha.com.myorganizer.model.MOTask;
 import avisha.com.myorganizer.presenter.NewTaskPresenter;
+import avisha.com.myorganizer.util.Util;
 
-public class NewTaskActivity extends AppCompatActivity implements View.OnClickListener {
+public class NewTaskActivity extends AppCompatActivity {
 
     private static final int PERMISSION_WRITE_EXTERNAL = 1;
     private Button saveButton;
@@ -31,31 +42,70 @@ public class NewTaskActivity extends AppCompatActivity implements View.OnClickLi
     private EditText taskTimeView;
     private EditText taskPhoneView;
     private EditText taskEmailView;
+    private Calendar mCalendar;
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+            mCalendar.set(Calendar.YEAR, i);
+            mCalendar.set(Calendar.MONTH, i1);
+            mCalendar.set(Calendar.DATE, i2);
+            String textDate = Util.toTextDate(i, i1, i2);
+            taskDateView.setText(textDate);
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker timePicker, int i, int i1) {
+            mCalendar.set(Calendar.HOUR_OF_DAY, i);
+            mCalendar.set(Calendar.MINUTE, i1);
+            String textTime = Util.toTextTime(i, i1);
+            taskTimeView.setText(textTime);
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.task_edit, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_save:
+                if (validateAllFields()) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            PERMISSION_WRITE_EXTERNAL);
+                }
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getViewId();
         mPresenter = new NewTaskPresenter();
         mPresenter.attach(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.save_button: {
-                if (validateAllFields()) {
-                    ActivityCompat.requestPermissions(this,
-                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            PERMISSION_WRITE_EXTERNAL);
-                }
-            }
-            break;
-        }
     }
 
     @Override
@@ -77,9 +127,6 @@ public class NewTaskActivity extends AppCompatActivity implements View.OnClickLi
                 }
                 return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 
@@ -102,7 +149,7 @@ public class NewTaskActivity extends AppCompatActivity implements View.OnClickLi
         mMoTask.setImportant(taskImportantView.isChecked());
         mMoTask.setUrgent(taskUrgentView.isChecked());
 
-        mMoTask.setDate(new Date().getTime());
+        mMoTask.setDate(mCalendar.getTimeInMillis());
         mMoTask.setPhone(taskPhoneView.getText().toString());
         mMoTask.setEmail(taskEmailView.getText().toString());
 
@@ -118,11 +165,76 @@ public class NewTaskActivity extends AppCompatActivity implements View.OnClickLi
         taskPhoneView = (EditText) findViewById(R.id.phone_edit);
         taskEmailView = (EditText) findViewById(R.id.email_edit);
 
-        saveButton = (Button) findViewById(R.id.save_button);
-        saveButton.setOnClickListener(this);
+        mCalendar = Calendar.getInstance();
+        taskDateView.setText(Util.toTextDate(mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH), mCalendar.get(Calendar.DATE)));
+        taskDateView.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                DatePickerFragment newFragment = new DatePickerFragment();
+                                                newFragment.show(getSupportFragmentManager(), "datePicker");
+                                                newFragment.setOnDateSetListener(dateSetListener);
+                                            }
+                                        }
+
+        );
+
+        taskTimeView.setText(Util.toTextTime(mCalendar.get(Calendar.HOUR_OF_DAY), mCalendar.get(Calendar.MINUTE)));
+        taskTimeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerFragment newFragment = new TimePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "timePicker");
+                newFragment.setOnTimeSetListener(timeSetListener);
+            }
+        });
     }
 
     public void closeActivity() {
         finish();
+    }
+
+    public static class DatePickerFragment extends DialogFragment {
+
+        private DatePickerDialog.OnDateSetListener listener;
+        private DatePickerDialog datePickerDialog;
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            datePickerDialog = new DatePickerDialog(getActivity(), listener, year, month, day);
+            // Create a new instance of DatePickerDialog and return it
+            return datePickerDialog;
+        }
+
+        public void setOnDateSetListener(DatePickerDialog.OnDateSetListener listener) {
+            this.listener = listener;
+        }
+
+    }
+
+    public static class TimePickerFragment extends DialogFragment {
+
+        private TimePickerDialog.OnTimeSetListener listener;
+        private TimePickerDialog timePickerDialog;
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int hourofDay = c.get(Calendar.HOUR_OF_DAY);
+            int minutes = c.get(Calendar.MINUTE);
+            timePickerDialog = new TimePickerDialog(getActivity(), listener, hourofDay, minutes, false);
+            // Create a new instance of DatePickerDialog and return it
+            return timePickerDialog;
+        }
+
+        public void setOnTimeSetListener(TimePickerDialog.OnTimeSetListener listener) {
+            this.listener = listener;
+        }
+
     }
 }
